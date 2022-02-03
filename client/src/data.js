3051babf708ed81450b108145bae897065e66537
Work <work@Works-Mac-mini.local>
2022-02-03 03:26:36 -0800

    make it better

---
+++ client/src/data.js
@@ -0,0 +1,65 @@
+import hero from "./assets/hero.jpg";
+import soaps from "./assets/soaps.jpg";
+import swag from "./assets/swag.jpg";
+import lotions from "./assets/lotions.jpg";
+import citrus from "./assets/citrus.png";
+import botanical from "./assets/botanical.png";
+import charcoal from "./assets/charcoal.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: hero,
+    title: "GET READY TO REFRESH",
+    desc: "Pair those champagne bubbles with the perfect soapy suds this New Year's.",
+    bg: "f5fafd",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: soaps,
+    title: "SOAPS",
+    cat: "Soaps",
+  },
+  {
+    id: 2,
+    img: swag,
+    title: "SWAG",
+    cat: "Swag",
+  },
+  {
+    id: 3,
+    img: lotions,
+    title: "LOTIONS",
+    cat: "Lotions",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: citrus,
+  },
+  {
+    id: 2,
+    img: botanical,
+  },
+  {
+    id: 3,
+    img: charcoal,
+  },
+  {
+    id: 4,
+    img: citrus,
+  },
+  {
+    id: 5,
+    img: botanical,
+  },
+  {
+    id: 6,
+    img: charcoal,
+  },
+];
