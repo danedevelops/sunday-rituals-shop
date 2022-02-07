31dad481e81850472a8cbd70be8081ed5c8b66ad
Work <work@Works-Mac-mini.local>
2022-02-06 20:07:16 -0800

    please

--- index.js
+++ index.js
@@ -27,13 +27,11 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
