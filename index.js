e80fa2c1cfd8c07533d250fe60e9443cdaca057a
Work <work@Works-Mac-mini.local>
2022-02-06 17:19:01 -0800

    updated CORS

--- index.js
+++ index.js
@@ -18,9 +18,24 @@ mongoose
     console.log(err);
   });
 
-app.use(
-  cors({ origin: "https://sunday-rituals.herokuapp.com/", credentials: true })
-);
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
