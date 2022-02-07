0dd6065d3eb493a170516f9c5e7b2c86096657bb
Work <work@Works-Mac-mini.local>
2022-02-06 20:33:48 -0800

    test

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 `;
 
 const Products = ({ cat, filters, sort }) => {
-  const [products, setProducts] = useState([popularProducts]);
+  const [products, setProducts] = useState(popularProducts);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   // useEffect(() => {
